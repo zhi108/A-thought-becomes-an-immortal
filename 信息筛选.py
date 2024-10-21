8cbee479e8221f465c6f3efce703e463cd8d7f37
zhi108 <129959372+zhi108@users.noreply.github.com>
2024-10-21 22:18:48 +0800

    Add files via upload

--- 信息筛选.py
+++ 信息筛选.py
@@ -11,7 +11,7 @@ interval = 5
 keyboard_controller = Controller()
 
 # 定义药材品阶
-grades = ["九品药材", "八品药材", "七品药材", "六品药材", "五品药材", "四品药材", "三品药材", "二品药材", "一品药材"]
+grades = ["九品药材", "八品药材", "七品药材"]#, "六品药材", "五品药材", "四品药材", "三品药材", "二品药材", "一品药材"]
 
 # 等待几秒钟以便你有时间切换到QQ聊天窗口
 time.sleep(5)
@@ -22,7 +22,7 @@ def send_message_and_get_response(message):
     pyautogui.click(x=542, y=805)
     # 输入@一念
     keyboard_controller.type("@")
-    time.sleep(0.5)
+    time.sleep(1)
     pyautogui.click(x=812, y=761)
     # 输入消息
     for char in message:
@@ -34,10 +34,10 @@ def send_message_and_get_response(message):
     pyautogui.press('enter')
 
     # 等待响应返回
-    time.sleep(2)
+    time.sleep(3)
     pyautogui.click(x=594, y=703)
     # 复制返回的背包信息
-    pyautogui.hotkey('ctrl', 'a')
+    # pyautogui.hotkey('ctrl', 'a')
     pyautogui.hotkey('ctrl', 'c')
 
     # 从剪贴板获取返回的背包信息
@@ -73,6 +73,19 @@ def send_refresh_message(herb_name):
     return response
 
 
+def list_herb(herb_name, price):
+    # 将价格从万转化为具体数字
+    numeric_price = int(price * 10000 - 200000)
+    for _ in range(3):  # 每种药材上架三次
+        message = f"坊市上架 {herb_name} {numeric_price}"
+        send_message_and_get_response(message)
+        # 检查剪贴板内容是否包含"每人只可上架十五个物品！"
+        clipboard_content = pyperclip.paste()
+        if "每人只可上架十五个物品！" in clipboard_content:
+            print("已达到上架限制，停止运行。")
+            return
+
+
 # 主循环
 all_items = []
 
@@ -88,12 +101,14 @@ for grade in grades:
         prices = extract_prices(refresh_response)
         for price in prices:
             all_items.append((name, price))
-    # 将价格从高到低进行排序并输出
-    sorted_items = sorted(all_items, key=lambda x: x[1], reverse=True)
-    print("从高到低排序的价格：")
-    for item in sorted_items:
-        print(f"{item[0]}: {item[1]}万")
-    time.sleep(interval)
+
+# 将价格从高到低进行排序并输出
+sorted_items = sorted(all_items, key=lambda x: x[1], reverse=True)
+print("从高到低排序的价格：")
+for item in sorted_items:
+    print(f"{item[0]}: {item[1]}万")
+    list_herb(item[0], item[1])  # 上架药材
+time.sleep(interval)
 
 
 
